92dd0e876a3867933f093f57f42345b243646eba
Fabio Mascarenhas <mascarenhas@lambda-2.local>
2010-03-29 16:10:38 -0300

    change default bin_dir in luarocks lfw to LFW_ROOT instead of LFW_ROOT\utils

--- lfw/luarocks_config.lua
+++ lfw/luarocks_config.lua
@@ -4,7 +4,7 @@ rocks_servers = {
 }
 rocks_trees = {
    { root = LFW_ROOT, rocks_dir = LFW_ROOT..[[\rocks]],
-     bin_dir = LFW_ROOT..[[\utils]], lua_dir = LFW_ROOT..[[\lua]],
+     bin_dir = LFW_ROOT, lua_dir = LFW_ROOT..[[\lua]],
      lib_dir = LFW_ROOT..[[\clibs]] }
 }
-variables.WRAPPER = LFW_ROOT..[[rclauncher.obj]]
+variables.WRAPPER = LFW_ROOT..[[\rclauncher.obj]]
