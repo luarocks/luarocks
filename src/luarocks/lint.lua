496dc3d8ea8d65082dcfb6d5aa44353c3fd0433d
Hisham Muhammad <hisham@gobolinux.org>
2012-09-18 20:55:57 -0300

    Add 'lint' command, to check for rockspec validity.

---
+++ src/luarocks/lint.lua
@@ -0,0 +1,37 @@
+
+--- Module implementing the LuaRocks "lint" command.
+-- Utility function that checks syntax of the rockspec.
+module("luarocks.lint", package.seeall)
+
+local util = require("luarocks.util")
+local download = require("luarocks.download")
+local fetch = require("luarocks.fetch")
+
+help_summary = "Check syntax of a rockspec."
+help_arguments = "<rockspec>"
+help = [[
+This is a utility function that checks the syntax of a rockspec.
+
+It reports success or failure if the text of a rockspec is
+syntactically correct.
+]]
+
+function run(...)
+   local flags, input = util.parse_flags(...)
+   
+   local filename = input
+   if not input:match(".rockspec$") then
+      local err
+      filename, err = download.download("rockspec", input)
+      if not filename then
+         return nil, err
+      end
+   end
+
+   local rs, err = fetch.load_local_rockspec(filename)
+   if not rs then
+      return nil, "Failed loading rockspec: "..err
+   end
+
+   return true
+end
