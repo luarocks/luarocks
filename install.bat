be6664eea148ef2fc24c29ed812b5c2966f6a121
Philipp Janda <siffiejoe@gmx.net>
2013-09-29 09:23:25 +0200

    Lua 5.2 paths in wrapper scripts on windows

--- install.bat
+++ install.bat
@@ -508,6 +508,7 @@ for _, c in ipairs{"luarocks", "luarocks-admin"} do
 @ECHO OFF
 SETLOCAL
 SET LUA_PATH=$LUADIR\?.lua;$LUADIR\?\init.lua;%LUA_PATH%
+IF NOT "%LUA_PATH_5_2%"=="" SET LUA_PATH_5_2=$LUADIR\?.lua;$LUADIR\?\init.lua;%LUA_PATH_5_2%
 SET PATH=$BINDIR\;%PATH%
 "$LUA_INTERPRETER" "$BINDIR\]]..c..[[.lua" %*
 ENDLOCAL
