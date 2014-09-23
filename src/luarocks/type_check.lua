86ba23c8a52a37dce829e3fa8b21f32fbb45a6a3
mpeterv <mpeterval@gmail.com>
2014-09-23 21:59:58 +0400

    Fix `luarocks lint`.
    Now it does not crash on rockspecs with malformed version.

--- src/luarocks/type_check.lua
+++ src/luarocks/type_check.lua
@@ -216,7 +216,7 @@ local function type_check_item(version, item, typetbl, context)
       end
       if typetbl._pattern then
          if not item:match("^"..typetbl._pattern.."$") then
-            return nil, "Type mismatch on field "..context..": invalid value "..item.." does not match '"..expected.."'"
+            return nil, "Type mismatch on field "..context..": invalid value "..item.." does not match '"..typetbl._pattern.."'"
          end
       end
    elseif expected_type == "table" then
