49c03d4f85c841860ea52fa752b6d5d15264e45f
Ignacio Burgueño <ignaciob@inconcertcc.com>
2016-02-01 11:09:04 -0300

    Address some issues raised in review

--- src/luarocks/shell.lua
+++ src/luarocks/shell.lua
@@ -17,22 +17,25 @@ configured by the LuaRocks installation.
 --- Driver function for "shell" command.
 -- @return boolean This function always succeeds.
 function shell_cmd.run(...)
-   local lr_path, lr_cpath = cfg.package_paths()
+   local lr_path, lr_cpath, lr_bin = cfg.package_paths()
    local path_sep = cfg.export_path_separator
    
    lr_path =  lr_path.. ";" .. package.path
    lr_cpath = lr_cpath .. ";" .. package.cpath
+   lr_bin = lr_bin .. path_sep .. os.getenv("PATH")
 
    lr_path = cfg.export_lua_path:format(util.remove_path_dupes(lr_path, ';'))
    lr_cpath = cfg.export_lua_cpath:format(util.remove_path_dupes(lr_cpath, ';'))
+   lr_bin = cfg.export_path:format(util.remove_path_dupes(lr_bin, path_sep))
 
    local shell, cmd = os.getenv("SHELL")
    if shell then
-      cmd = ([[LUA_PATH="%s" LUA_CPATH="%s" "%s"]]):format(lr_path, lr_cpath, shell)
+      cmd = ([[LUA_PATH="%s" LUA_CPATH="%s" PATH="%s" %s"]]):format(lr_path, lr_cpath, lr_bin, shell)
    else
-      cmd = ([[cmd /k "%s && %s"]]):format(lr_path, lr_cpath)
+      cmd = ([[cmd /k "%s && %s && %s"]]):format(lr_path, lr_cpath, lr_bin)
    end
-   return fs.execute(cmd)
+   fs.execute(cmd)
+   return true
 end
 
 return shell_cmd
