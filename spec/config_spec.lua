51c036af563f4df3d5a5cc8cc7a7f04fdd1a6072
roboo <robo.karasek@gmail.com>
2016-06-26 23:25:11 +0200

    New tests

--- spec/config_spec.lua
+++ spec/config_spec.lua
@@ -6,6 +6,7 @@ expose("LuaRocks config tests #blackbox #b_config", function()
    
    before_each(function()
       test_env.setup_specs(extra_rocks)
+      testing_paths = test_env.testing_paths
       run = test_env.run
    end)
 
@@ -34,5 +35,38 @@ expose("LuaRocks config tests #blackbox #b_config", function()
    end)
    it("LuaRocks config missing user config", function()
       assert.is_false(run.luarocks_bool("config --user-config", {LUAROCKS_CONFIG = "missing_file.lua"}))
+   end)
+
+
+   it("LuaRocks fail system config", function()
+      os.remove(testing_paths.testing_lrprefix .. "/etc/luarocks/config.lua")
+      assert.is_false(run.luarocks_bool("config --system-config;"))
+   end)
+   it("LuaRocks system config", function()
+      local scdir = testing_paths.testing_lrprefix .. "/etc/luarocks"
+      lfs.mkdir(testing_paths.testing_lrprefix)
+      lfs.mkdir(testing_paths.testing_lrprefix .. "/etc/")
+      lfs.mkdir(scdir)
+
+      local sysconfig = io.open(scdir .. "/config.lua", "w+")
+      sysconfig:write(" ")
+      sysconfig:close()
+
+      local output = run.luarocks("config --system-config;")
+      assert.are.same(output, scdir .. "/config.lua")
+      test_env.remove_dir(testing_paths.testing_lrprefix)
+   end)
+   it("LuaRocks fail system config invalid #speci", function()
+      local scdir = testing_paths.testing_lrprefix .. "/etc/luarocks"
+      lfs.mkdir(testing_paths.testing_lrprefix)
+      lfs.mkdir(testing_paths.testing_lrprefix .. "/etc/")
+      lfs.mkdir(scdir)
+
+      local sysconfig = io.open(scdir .. "/config.lua", "w+")
+      sysconfig:write("if if if")
+      sysconfig:close()
+
+      assert.is_false(run.luarocks_bool("config --system-config;"))
+      test_env.remove_dir(testing_paths.testing_lrprefix)
    end)
 end)
