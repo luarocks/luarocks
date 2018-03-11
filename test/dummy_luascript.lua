66a286cd6f49e1ab60b0f993e5fc1e64f7082ec0
Karthik Nair <nairkarthik1997@gmail.com>
2018-03-12 04:40:20 +0530

    Added a dummy script

---
+++ test/dummy_luascript.lua
@@ -0,0 +1,11 @@
+var = 13
+
+if var>13 then
+   print("Hi i have puberty")
+end
+elseif var==13 then
+   print("I will have puberty next year")
+end
+else
+   print("I too young to have puberty") 
+end
