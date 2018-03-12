161c082814ae7a57d7ac4df00af84278e6418669
Karthik Nair <nairkarthik1997@gmail.com>
2018-03-13 05:11:05 +0530

    Update dummy_luascript.lua

--- test/dummy_luascript.lua
+++ test/dummy_luascript.lua
@@ -2,10 +2,8 @@ var = 18
 
 if var>18 then
    print("Racheal gets 2 chocolates")
-end
 elseif var==18 then
    print("Racheal gets 1 chocolate")
-end
 else
    print("Racheal gets no chocolates") 
 end
