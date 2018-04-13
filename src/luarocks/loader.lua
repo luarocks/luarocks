87cb13156f767cff04daee44d0a5c4486fa402da
Hisham Muhammad <hisham@gobolinux.org>
2018-04-13 12:30:21 -0300

    Use a single load_manifest function throughout the program

--- src/luarocks/loader.lua
+++ src/luarocks/loader.lua
@@ -58,7 +58,7 @@ local function load_rocks_trees()
    local any_ok = false
    local trees = {}
    for _, tree in ipairs(cfg.rocks_trees) do
-      local manifest, err = manif.load_local_manifest(path.rocks_dir(tree))
+      local manifest, err = manif.fast_load_local_manifest(path.rocks_dir(tree))
       if manifest then
          any_ok = true
          table.insert(trees, {tree=tree, manifest=manifest})
