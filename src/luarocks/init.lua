e909b313c19f32e578ef9d34f53bd7053b9a4084
Jitesh Pabla <jiteshpabla97@gmail.com>
2018-05-31 21:35:48 +0530

    init.lua: Add list function which returns a table of installed rocks

--- src/luarocks/init.lua
+++ src/luarocks/init.lua
@@ -18,11 +18,53 @@ function luarocks.test_func()
 end
 
 --- Print the list of installed rocks
-local list = require("luarocks.cmd.list")
-flags = {}
-function luarocks.list(flags)
-       list.command(flags)
-end
+--local list = require("luarocks.cmd.list")
+--flags = {}
+--function luarocks.list(flags)
+--	list.command(flags)
+--end
 
+
+local search = require("luarocks.search")
+local vers = require("luarocks.vers")
+--local cfg = require("luarocks.core.cfg")
+local util = require("luarocks.util")
+local path = require("luarocks.path")
+
+
+--- Return a table of installed rocks
+function luarocks.list(...)
+
+	---[[ here there should be a function that parses the ... into flags, filter, version]]
+
+	--- hardcode flags empty for now
+	flags = {}
+
+	local query = search.make_query(filter and filter:lower() or "", version)
+	query.exact_name = false
+	local trees = cfg.rocks_trees
+	if flags["tree"] then
+	  trees = { flags["tree"] }
+	end
+
+	--if flags["outdated"] then
+	--   return list_outdated(trees, query, flags["porcelain"])
+	--end
+
+	local results = {}
+	for _, tree in ipairs(trees) do
+	  local ok, err, errcode = search.manifest_search(results, path.rocks_dir(tree), query)
+	  if not ok and errcode ~= "open" then
+	     util.warning(err)
+	  end
+	end
+
+	--util.title("Installed rocks:", flags["porcelain"])
+	--results = search.return_results(results, flags["porcelain"])
+
+	--- hardcoding flags["porcelain"] to true
+	results = search.return_results(results, true)
+	return results
+end
 
 return luarocks
