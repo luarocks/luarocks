ac84da2ff18d669269f26b20e7fe52f45db6ddb8
Jitesh Pabla <jiteshpabla97@gmail.com>
2018-06-01 01:34:01 +0530

    init.lua: Add check_outdated function

--- src/luarocks/init.lua
+++ src/luarocks/init.lua
@@ -21,6 +21,37 @@ function luarocks.test_func()
 	return 1
 end
 
+--- Return table of outdated installed rocks
+-- called only by list() function
+local function check_outdated(trees, query)
+   local results_installed = {}
+   for _, tree in ipairs(trees) do
+      search.manifest_search(results_installed, path.rocks_dir(tree), query)
+   end
+   local outdated = {}
+   for name, versions in util.sortedpairs(results_installed) do
+      versions = util.keys(versions)
+      table.sort(versions, vers.compare_versions)
+      local latest_installed = versions[1]
+
+      local query_available = search.make_query(name:lower())
+      query.exact_name = true
+      local results_available, err = search.search_repos(query_available)
+      
+      if results_available[name] then
+         local available_versions = util.keys(results_available[name])
+         table.sort(available_versions, vers.compare_versions)
+         local latest_available = available_versions[1]
+         local latest_available_repo = results_available[name][latest_available][1].repo
+         
+         if vers.compare_versions(latest_available, latest_installed) then
+            table.insert(outdated, { name = name, installed = latest_installed, available = latest_available, repo = latest_available_repo })
+         end
+      end
+   end
+   return outdated
+end
+
 --- Return a table of installed rocks
 function luarocks.list(filter, outdated, version, tree)
    local query = search.make_query(filter and filter:lower() or "", version)
