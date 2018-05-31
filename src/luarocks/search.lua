0449755c8357824b3c49d345b1a48f84e36af1b1
Jitesh Pabla <jiteshpabla97@gmail.com>
2018-05-31 21:37:16 +0530

    search.lua: Add return_results function, to return table of installed rocks

--- src/luarocks/search.lua
+++ src/luarocks/search.lua
@@ -327,6 +327,43 @@ function search.find_suitable_rock(query)
    end
 end
 
+--- Return a list of rocks/rockspecs  as a table.
+-- @param results table: A table where keys are package names and versions
+-- are tables matching version strings to an array of rocks servers.
+-- @param porcelain boolean or nil: A flag to force machine-friendly output.
+function search.return_results(results, porcelain)
+   assert(type(results) == "table")
+   assert(type(porcelain) == "boolean" or not porcelain)
+
+   --- iterator for results table
+   i=1
+   for package, versions in util.sortedpairs(results) do
+      --if not porcelain then
+      --   util.printout(package)
+      --end
+      
+      for version, repos in util.sortedpairs(versions, vers.compare_versions) do
+         for _, repo in ipairs(repos) do
+            repo.repo = dir.normalize(repo.repo)
+            if porcelain then
+               --util.printout(package, version, repo.arch, repo.repo)
+               results[i] = {package, version, repo.arch, repo.repo}
+            else
+               --util.printout("   "..version.." ("..repo.arch..") - "..repo.repo)
+               results[i] = {"   ", version, repo.arch, repo.repo}
+            end
+         end
+      end
+      --if not porcelain then
+      --   util.printout()
+      --end
+      i = i+1
+      --print(i)
+
+   end
+   return results
+end
+
 --- Print a list of rocks/rockspecs on standard output.
 -- @param results table: A table where keys are package names and versions
 -- are tables matching version strings to an array of rocks servers.
