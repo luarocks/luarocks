8f4f240c165b72858db6d45b45c621c9aae695de
Jitesh Pabla <jiteshpabla97@gmail.com>
2018-06-02 17:13:13 +0530

    init.lua: Add initial version of doc function

--- src/luarocks/init.lua
+++ src/luarocks/init.lua
@@ -2,11 +2,14 @@
 local luarocks = {}
 
 local cfg = require("luarocks.core.cfg")
-local list = require("luarocks.cmd.list")
 local search = require("luarocks.search")
 local vers = require("luarocks.vers")
 local util = require("luarocks.util")
 local path = require("luarocks.path")
+local dir = require("luarocks.dir")
+local fetch = require("luarocks.fetch")
+local fs = require("luarocks.fs")
+local download = require("luarocks.download")
 
 --- Obtain version of LuaRocks and its API.
 -- @return (string, string) Full version of this LuaRocks instance
@@ -74,6 +77,121 @@ function luarocks.list(filter, outdated, version, tree)
    end
    results = search.return_results(results)
    return results
+end
+
+local function replace_tree(flags, tree)
+   tree = dir.normalize(tree)
+   --flags["tree"] = tree
+   path.use_tree(tree)
+end
+
+local function try_to_get_homepage(name, version)
+   local temp_dir, err = fs.make_temp_dir("doc-"..name.."-"..(version or ""))
+   if not temp_dir then
+      return nil, "Failed creating temporary directory: "..err
+   end
+   util.schedule_function(fs.delete, temp_dir)
+   local ok, err = fs.change_dir(temp_dir)
+   if not ok then return nil, err end
+   local filename, err = download.download("rockspec", name, version)
+   if not filename then return nil, err end
+   local rockspec, err = fetch.load_local_rockspec(filename)
+   if not rockspec then return nil, err end
+   fs.pop_dir()
+   local descript = rockspec.description or {}
+   if not descript.homepage then return nil, "No homepage defined for "..name end
+   return descript.homepage, nil, nil
+end
+
+--- Return homepage and doc file names of an installed rock
+function luarocks.doc(name, version)
+
+   --- The following code has been copied from command_line.lua; because without this, an error was poppong up:
+   --[[ lua: ...a/vert/api1_sandbox/share/lua/5.2/luarocks/core/path.lua:17: assertion failed!
+   stack traceback:
+      [C]: in function 'assert'
+      ...a/vert/api1_sandbox/share/lua/5.2/luarocks/core/path.lua:17: in function 'rocks_dir'
+      ...la/lua/vert/api1_sandbox/share/lua/5.2/luarocks/path.lua:78: in function 'install_dir'
+      ...la/lua/vert/api1_sandbox/share/lua/5.2/luarocks/path.lua:230: in function 'configure_paths'
+      ...a/lua/vert/api1_sandbox/share/lua/5.2/luarocks/fetch.lua:275: in function 'load_local_rockspec'
+      ...la/lua/vert/api1_sandbox/share/lua/5.2/luarocks/init.lua:124: in function 'doc'
+      api_testing.lua:29: in main chunk
+      [C]: in ?
+   --]]
+   -- because in path.install_dir, cfg.root_dir is called, which has to be specified, which the code below does.
+   if 1 then
+      local named = false
+      for _, tree in ipairs(cfg.rocks_trees) do
+         if type(tree) == "table" then
+            if not tree.root then
+               die("Configuration error: tree '"..tree.name.."' has no 'root' field.")
+            end
+            replace_tree(flags, tree.root)
+            named = true
+            break
+         end
+      end
+      if not named then
+         local root_dir = fs.absolute_name(flags["tree"])
+         replace_tree(flags, root_dir)
+      end
+   elseif flags["local"] then
+      if not cfg.home_tree then
+         die("The --local flag is meant for operating in a user's home directory.\n"..
+             "You are running as a superuser, which is intended for system-wide operation.\n"..
+             "To force using the superuser's home, use --tree explicitly.")
+      end
+      replace_tree(flags, cfg.home_tree)
+   else
+      local trees = cfg.rocks_trees
+      path.use_tree(trees[#trees])
+   end
+   
+   if type(cfg.root_dir) == "string" then
+      cfg.root_dir = cfg.root_dir:gsub("/+$", "")
+   else
+      cfg.root_dir.root = cfg.root_dir.root:gsub("/+$", "")
+   end
+   -- command_line.lua copied code ends here
+
+   if not name then
+      return nil, "Argument missing. "
+   end
+
+   name = name:lower()
+
+   -- for now i can do away with flags["tree"]
+   --local iname, iversion, repo = search.pick_installed_rock(name, version, flags["tree"])
+   local iname, iversion, repo = search.pick_installed_rock(name, version)
+   if not iname then
+      return try_to_get_homepage(name, version)
+   end
+
+   name, version = iname, iversion
+   
+   local rockspec, err = fetch.load_local_rockspec(path.rockspec_file(name, version, repo))
+   if not rockspec then return nil,err end
+   local descript = rockspec.description or {}
+
+   local directory = path.install_dir(name,version,repo)
+   
+   local docdir
+   local directories = { "doc", "docs" }
+   for _, d in ipairs(directories) do
+      local dirname = dir.path(directory, d)
+      if fs.is_dir(dirname) then
+         docdir = dirname
+         break
+      end
+   end
+
+   docdir = dir.normalize(docdir):gsub("/+", "/")
+   local files = fs.find(docdir)
+   local htmlpatt = "%.html?$"
+   local extensions = { htmlpatt, "%.md$", "%.txt$",  "%.textile$", "" }
+   local basenames = { "index", "readme", "manual" }
+   
+   return descript.homepage, docdir, files
 end
 
 return luarocks
