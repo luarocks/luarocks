ac7e9ad877b318fcb9985e7ed30b056ee85815ac
George Roman <george.roman.99@gmail.com>
2018-07-30 12:23:00 +0300

    Reorganize the search API

--- src/luarocks/cmd/search.lua
+++ src/luarocks/cmd/search.lua
@@ -1,13 +1,11 @@
-
 --- Module implementing the LuaRocks "search" command.
 -- Queries LuaRocks servers.
 local cmd_search = {}
 
 local cfg = require("luarocks.core.cfg")
-local util = require("luarocks.util")
+local luarocks = require("luarocks.init")
 local search = require("luarocks.search")
-local queries = require("luarocks.queries")
-local results = require("luarocks.results")
+local util = require("luarocks.util")
 
 cmd_search.help_summary = "Query the LuaRocks servers."
 cmd_search.help_arguments = "[--source] [--binary] { <name> [<version>] | --all }"
@@ -21,60 +19,40 @@ cmd_search.help = [[
 --porcelain  Return a machine readable format.
 ]]
 
---- Splits a list of search results into two lists, one for "source" results
--- to be used with the "build" command, and one for "binary" results to be
--- used with the "install" command.
--- @param result_tree table: A search results table.
--- @return (table, table): Two tables, one for source and one for binary
--- results.
-local function split_source_and_binary_results(result_tree)
-   local sources, binaries = {}, {}
-   for name, versions in pairs(result_tree) do
-      for version, repositories in pairs(versions) do
-         for _, repo in ipairs(repositories) do
-            local where = sources
-            if repo.arch == "all" or repo.arch == cfg.arch then
-               where = binaries
-            end
-            local entry = results.new(name, version, repo.repo, repo.arch)
-            search.store_result(where, entry)
-         end
-      end
-   end
-   return sources, binaries
-end
-
 --- Driver function for "search" command.
 -- @param name string: A substring of a rock name to search.
 -- @param version string or nil: a version may also be passed.
--- @return boolean or (nil, string): True if build was successful; nil and an
+-- @return boolean or (nil, string): True if the search was successful; nil and an
 -- error message otherwise.
 function cmd_search.command(flags, name, version)
-
    name = util.adjust_name_and_namespace(name, flags)
 
    if flags["all"] then
-      name, version = "", nil
+      name = nil
    end
 
    if type(name) ~= "string" and not flags["all"] then
-      return nil, "Enter name and version or use --all. "..util.see_help("search")
+      return nil, "Enter name and version or use --all. " .. util.see_help("search")
    end
    
-   local query = queries.new(name:lower(), version, true)
-   local result_tree, err = search.search_repos(query)
    local porcelain = flags["porcelain"]
-   local full_name = name .. (version and " " .. version or "")
-   util.title(full_name .. " - Search results for Lua "..cfg.lua_version..":", porcelain, "=")
-   local sources, binaries = split_source_and_binary_results(result_tree)
-   if next(sources) and not flags["binary"] then
+   local full_name = (name or "") .. (version and " " .. version or "")
+   util.title(full_name .. " - Search results for Lua " .. cfg.lua_version .. ":", porcelain, "=")
+
+   local search_table, err = luarocks.search(name, version, (flags["source"] and "source") or (flags["binary"] and "binary")) 
+   if not search_table then
+      return nil, err
+   end
+
+   if search_table["sources"] and not flags["binary"] then
       util.title("Rockspecs and source rocks:", porcelain)
-      search.print_result_tree(sources, porcelain)
+      search.print_result_tree(search_table["sources"], porcelain)
    end
-   if next(binaries) and not flags["source"] then    
+   if search_table["binary"] and not flags["source"] then    
       util.title("Binary and pure-Lua rocks:", porcelain)
-      search.print_result_tree(binaries, porcelain)
+      search.print_result_tree(search_table["binary"], porcelain)
    end
+
    return true
 end
 
--- src/luarocks/init.lua
+++ src/luarocks/init.lua
@@ -1,9 +1,12 @@
 --- LuaRocks public programmatic API, version 3.0
 local luarocks = {}
 
-local doc = require("luarocks.doc") 
-luarocks.doc = doc.doc
-luarocks.homepage = doc.homepage
+local doc_api = require("luarocks.api.doc") 
+luarocks.doc = doc_api.doc
+luarocks.homepage = doc_api.homepage
+
+local search_api = require("luarocks.api.search")
+luarocks.search = search_api.search
 
 local cfg = require("luarocks.core.cfg")
 local queries = require("luarocks.queries")
@@ -262,51 +265,6 @@ function luarocks.show(name, version, tree)
       end
    end
    return show_table
-end
-
---- Splits a list of search results into two lists, one for "source" results
--- to be used with the "build" command, and one for "binary" results to be
--- used with the "install" command.
--- @param result_tree table: A search results table.
--- @return (table, table): Two tables, one for source and one for binary
--- results.
-local function split_source_and_binary_results(result_tree)
-   local sources, binaries = {}, {}
-   for name, versions in pairs(result_tree) do
-      for version, repositories in pairs(versions) do
-         for _, repo in ipairs(repositories) do
-            local where = sources
-            if repo.arch == "all" or repo.arch == cfg.arch then
-               where = binaries
-            end
-            local entry = results.new(name, version, repo.repo, repo.arch)
-            search.store_result(where, entry)
-         end
-      end
-   end
-   return sources, binaries
-end
-
---- Return a table of queried rocks from LuaRocks servers
-function luarocks.search(name, version, binary_or_source)
-   local search_table = {}
-
-   if not name then
-      name, version = "", nil
-   end
-
-   local query = queries.new(name:lower(), version, true)
-   local result_tree, err = search.search_repos(query)
-   local sources, binaries = split_source_and_binary_results(result_tree)
-   if binary_or_source == nil then
-        search_table["sources"] = sources
-        search_table["binary"] =  binary
-   elseif next(sources) and (binary_or_source == "source") then
-      search_table["sources"] = sources
-   elseif next(binaries) and (binary_or_source == "binary") then
-      search_table["binary"] =  binary
-   end
-   return search_table
 end
 
 --- force = nil for not forcing
