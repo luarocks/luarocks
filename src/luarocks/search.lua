91ecea77c6e01e5bf3e0a15ea2146c0a0dbcd573
Paul Ouellette <oue.paul18@gmail.com>
2019-08-12 15:31:14 -0400

    Fix wrong versions in search failure error message

--- src/luarocks/search.lua
+++ src/luarocks/search.lua
@@ -108,7 +108,7 @@ local function manifest_search(result_tree, repo, query, lua_version, is_local)
       repo = repo .. "/manifests/" .. query.namespace
    end
 
-   local manifest, err, errcode = manif.load_manifest(repo, lua_version)
+   local manifest, err, errcode = manif.load_manifest(repo, lua_version, true)
    if not manifest then
       return nil, err, errcode
    end
