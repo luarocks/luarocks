luarocks/luarocks

commit 0823c4dd62e15515ad1e77cd20681b720485c564

Move remaining commands to argparse

src/luarocks/cmd/path.lua
@@ -7,48 +7,46 @@ local util = require("luarocks.util")
 local cfg = require("luarocks.core.cfg")
 local fs = require("luarocks.fs")
 
-path_cmd.help_summary = "Return the currently configured package path."
-path_cmd.help_arguments = ""
-path_cmd.help = [[
+function path_cmd.add_to_parser(parser)
+   local cmd = parser:command("path", [[
 Returns the package path currently configured for this installation
 of LuaRocks, formatted as shell commands to update LUA_PATH and LUA_CPATH. 
-
---no-bin       Do not export the PATH variable
-
---append       Appends the paths to the existing paths. Default is to prefix
-               the LR paths to the existing paths.
-
---lr-path      Exports the Lua path (not formatted as shell command)
-
---lr-cpath     Exports the Lua cpath (not formatted as shell command)
-
---lr-bin       Exports the system path (not formatted as shell command)
-
 
 On Unix systems, you may run: 
   eval `luarocks path`
 And on Windows: 
-  luarocks path > "%temp%\_lrp.bat" && call "%temp%\_lrp.bat" && del "%temp%\_lrp.bat"
-]]
+  luarocks path > "%temp%\_lrp.bat" && call "%temp%\_lrp.bat" && del "%temp%\_lrp.bat"]],
+  util.see_also())
+      :summary("Return the currently configured package path.")
+      :add_help(false)
+
+   cmd:flag("--no-bin", "Do not export the PATH variable.")
+   cmd:flag("--append", "Appends the paths to the existing paths. Default is "..
+      "to prefix the LR paths to the existing paths.")
+   cmd:flag("--lr-path", "Exports the Lua path (not formatted as shell command).")
+   cmd:flag("--lr-cpath", "Exports the Lua cpath (not formatted as shell command).")
+   cmd:flag("--lr-bin", "Exports the system path (not formatted as shell command).")
+   cmd:flag("--bin"):hidden(true)
+end
 
 --- Driver function for "path" command.
 -- @return boolean This function always succeeds.
-function path_cmd.command(flags)
-   local lr_path, lr_cpath, lr_bin = cfg.package_paths(flags["tree"])
+function path_cmd.command(args)
+   local lr_path, lr_cpath, lr_bin = cfg.package_paths(args["tree"])
    local path_sep = cfg.export_path_separator
 
-   if flags["lr-path"] then
+   if args["lr_path"] then
       util.printout(util.cleanup_path(lr_path, ';', cfg.lua_version))
       return true
-   elseif flags["lr-cpath"] then
+   elseif args["lr_cpath"] then
       util.printout(util.cleanup_path(lr_cpath, ';', cfg.lua_version))
       return true
-   elseif flags["lr-bin"] then
+   elseif args["lr_bin"] then
       util.printout(util.cleanup_path(lr_bin, path_sep))
       return true
    end
    
-   if flags["append"] then
+   if args["append"] then
       lr_path = package.path .. ";" .. lr_path
       lr_cpath = package.cpath .. ";" .. lr_cpath
       lr_bin = os.getenv("PATH") .. path_sep .. lr_bin
@@ -60,10 +58,10 @@ function path_cmd.command(flags)
    
    local lpath_var, lcpath_var = util.lua_path_variables()
 
-   util.printout(fs.export_cmd(lpath_var, util.cleanup_path(lr_path, ';', cfg.lua_version, flags["append"])))
-   util.printout(fs.export_cmd(lcpath_var, util.cleanup_path(lr_cpath, ';', cfg.lua_version, flags["append"])))
-   if not flags["no-bin"] then
-      util.printout(fs.export_cmd("PATH", util.cleanup_path(lr_bin, path_sep, nil, flags["append"])))
+   util.printout(fs.export_cmd(lpath_var, util.cleanup_path(lr_path, ';', cfg.lua_version)))
+   util.printout(fs.export_cmd(lcpath_var, util.cleanup_path(lr_cpath, ';', cfg.lua_version)))
+   if not args["no_bin"] then
+      util.printout(fs.export_cmd("PATH", util.cleanup_path(lr_bin, path_sep)))
    end
    return true
 end

src/bin/luarocks
@@ -9,7 +9,6 @@ local cmd = require("luarocks.cmd")
 local description = "LuaRocks main command-line interface"
 
 local commands = {
-   help = "luarocks.cmd.help",
    init = "luarocks.cmd.init",
    pack = "luarocks.cmd.pack",
    unpack = "luarocks.cmd.unpack",

src/bin/luarocks-admin
@@ -9,7 +9,6 @@ local cmd = require("luarocks.cmd")
 local description = "LuaRocks repository administration interface"
 
 local commands = {
-   help = "luarocks.cmd.help",
    make_manifest = "luarocks.admin.cmd.make_manifest",
    add = "luarocks.admin.cmd.add",
    remove = "luarocks.admin.cmd.remove",

src/luarocks/admin/cmd/add.lua
@@ -11,20 +11,21 @@ local fs = require("luarocks.fs")
 local cache = require("luarocks.admin.cache")
 local index = require("luarocks.admin.index")
 
-add.help_summary = "Add a rock or rockspec to a rocks server."
-add.help_arguments = "[--server=<server>] [--no-refresh] {<rockspec>|<rock>...}"
-add.help = [[
-Arguments are local files, which may be rockspecs or rocks.
-The flag --server indicates which server to use.
-If not given, the default server set in the upload_server variable
-from the configuration file is used instead.
+function add.add_to_parser(parser)
+   local cmd = parser:command("add", "Add a rock or rockspec to a rocks server.",
+      util.see_also())
+      :add_help(false)
 
---no-refresh  The local cache should not be refreshed
-              prior to generation of the updated manifest.
---index       Produce an index.html file for the manifest.
-              This flag is automatically set if an index.html
-              file already exists.
-]]
+   cmd:argument("rock", "A local rockspec or rock file.")
+      :args("+")
+
+   cmd:option("--server", "The server to use. If not given, the default server "..
+      "set in the upload_server variable from the configuration file is used instead.")
+   cmd:flag("--no-refresh", "Do not refresh the local cache prior to "..
+      "generation of the updated manifest.")
+   cmd:flag("--index", "Produce an index.html file for the manifest. This "..
+      "flag is automatically set if an index.html file already exists.")
+end
 
 local function zip_manifests()
    for ver in util.lua_versions() do
@@ -124,14 +125,10 @@ local function add_files_to_server(refresh, rockfiles, server, upload_server, do
    return fs.execute(cmd)
 end
 
-function add.command(flags, ...)
-   local files = {...}
-   if #files < 1 then
-      return nil, "Argument missing. "..util.see_help("add", "luarocks-admin")
-   end
-   local server, server_table = cache.get_upload_server(flags["server"])
+function add.command(args)
+   local server, server_table = cache.get_upload_server(args["server"])
    if not server then return nil, server_table end
-   return add_files_to_server(not flags["no-refresh"], files, server, server_table, flags["index"])
+   return add_files_to_server(not args["no_refresh"], files, server, server_table, args["index"])
 end
 
 

src/luarocks/admin/cmd/make_manifest.lua
@@ -11,36 +11,37 @@ local deps = require("luarocks.deps")
 local fs = require("luarocks.fs")
 local dir = require("luarocks.dir")
 
-make_manifest.help_summary = "Compile a manifest file for a repository."
+function make_manifest.add_to_parser(parser)
+   local cmd = parser:command("make_manifest", "Compile a manifest file for a repository.", 
+      util.see_also())
+      :add_help(false)
 
-make_manifest.help = [[
-<argument>, if given, is a local repository pathname.
+   cmd:argument("repository", "Local repository pathname.")
+      :args("?")
 
---local-tree  If given, do not write versioned versions of the manifest file.
-              Use this when rebuilding the manifest of a local rocks tree.
-]]
+   cmd:flag("--local-tree", "If given, do not write versioned versions of the manifest file.\n"..
+      "Use this when rebuilding the manifest of a local rocks tree.")
+   cmd:option("--deps-mode"):hidden(true) -- TODO: Description?
+end
 
 --- Driver function for "make_manifest" command.
--- @param repo string or nil: Pathname of a local repository. If not given,
--- the default local repository configured as cfg.rocks_dir is used.
 -- @return boolean or (nil, string): True if manifest was generated,
 -- or nil and an error message.
-function make_manifest.command(flags, repo)
-   assert(type(repo) == "string" or not repo)
-   repo = repo or cfg.rocks_dir
+function make_manifest.command(args)
+   local repo = args.repository or cfg.rocks_dir
   
    util.printout("Making manifest for "..repo)
    
-   if repo:match("/lib/luarocks") and not flags["local-tree"] then
+   if repo:match("/lib/luarocks") and not args["local_tree"] then
       util.warning("This looks like a local rocks tree, but you did not pass --local-tree.")
    end
    
-   local ok, err = writer.make_manifest(repo, deps.get_deps_mode(flags), not flags["local-tree"])
-   if ok and not flags["local-tree"] then
+   local ok, err = writer.make_manifest(repo, deps.get_deps_mode(args), not args["local_tree"])
+   if ok and not args["local_tree"] then
       util.printout("Generating index.html for "..repo)
       index.make_index(repo)
    end
-   if flags["local-tree"] then
+   if args["local_tree"] then
       for luaver in util.lua_versions() do
          fs.delete(dir.path(repo, "manifest-"..luaver))
       end

src/luarocks/admin/cmd/refresh_cache.lua
@@ -3,18 +3,21 @@
 local refresh_cache = {}
 
 local cfg = require("luarocks.core.cfg")
+local util = require("luarocks.util")
 local cache = require("luarocks.admin.cache")
 
-refresh_cache.help_summary = "Refresh local cache of a remote rocks server."
-refresh_cache.help_arguments = "[--from=<server>]"
-refresh_cache.help = [[
-The flag --from indicates which server to use.
-If not given, the default server set in the upload_server variable
-from the configuration file is used instead.
-]]
+function refresh_cache.add_to_parser(parser)
+   local cmd = parser:command(
+      "refresh_cache", "Refresh local cache of a remote rocks server.", util.see_also())
+      :add_help(false)
 
-function refresh_cache.command(flags)
-   local server, upload_server = cache.get_upload_server(flags["server"])
+   cmd:option("--from", "The server to use. If not given, the default server "..
+      "set in the upload_server variable from the configuration file is used instead.")
+      :argname("<server>")
+end
+
+function refresh_cache.command(args)
+   local server, upload_server = cache.get_upload_server(args["server"])
    if not server then return nil, upload_server end
    local download_url = cache.get_server_urls(server, upload_server)
    

src/luarocks/admin/cmd/remove.lua
@@ -11,16 +11,19 @@ local fs = require("luarocks.fs")
 local cache = require("luarocks.admin.cache")
 local index = require("luarocks.admin.index")
 
-admin_remove.help_summary = "Remove a rock or rockspec from a rocks server."
-admin_remove.help_arguments = "[--server=<server>] [--no-refresh] {<rockspec>|<rock>...}"
-admin_remove.help = [[
-Arguments are local files, which may be rockspecs or rocks.
-The flag --server indicates which server to use.
-If not given, the default server set in the upload_server variable
-from the configuration file is used instead.
-The flag --no-refresh indicates the local cache should not be refreshed
-prior to generation of the updated manifest.
-]]
+function admin_remove.add_to_parser(parser)
+   local cmd = parser:command(
+      "remove", "Remove a rock or rockspec from a rocks server.", util.see_also())
+      :add_help(false)
+
+   cmd:argument("rock", "A local rockspec or rock file.")
+      :args("+")
+
+   cmd:option("--server", "The server to use. If not given, the default server "..
+      "set in the upload_server variable from the configuration file is used instead.")
+   cmd:flag("--no-refresh", "Do not refresh the local cache prior to "..
+      "generation of the updated manifest.")
+end
 
 local function remove_files_from_server(refresh, rockfiles, server, upload_server)
    assert(type(refresh) == "boolean" or not refresh)
@@ -76,14 +79,10 @@ local function remove_files_from_server(refresh, rockfiles, server, upload_serve
    return true
 end
 
-function admin_remove.command(flags, ...)
-   local files = {...}
-   if #files < 1 then
-      return nil, "Argument missing. "..util.see_help("remove", "luarocks-admin")
-   end
-   local server, server_table = cache.get_upload_server(flags["server"])
+function admin_remove.command(args)
+   local server, server_table = cache.get_upload_server(args["server"])
    if not server then return nil, server_table end
-   return remove_files_from_server(not flags["no-refresh"], files, server, server_table)
+   return remove_files_from_server(not args["no_refresh"], files, server, server_table)
 end
 
 

src/luarocks/cmd.lua
@@ -394,9 +394,7 @@ Variables:
    parser:option("--only-sources-from"):target("only_sources"):hidden(true)
 
    for _, module in util.sortedpairs(cmd_modules) do
-      if module.add_to_parser then -- TODO: Remove this check.
-         module.add_to_parser(parser)
-      end
+      module.add_to_parser(parser)
    end
 
    return parser

commit 7974374cd62bd3f8076f672c344dabadd7501d03

util: fix cleanup_path order
This change ensures that the package path fixups done by the bin wrapper take
effect.

Commit 72d80a35 had the default behavior inverted, and affected its use in
`luarocks.core.cfg`.

Commit 0823c4dd62e accidentally removed the use of `flags["append"]` in
`luarocks.cmd.path` and this slipped code review. This restores it.

src/luarocks/cmd/path.lua
@@ -35,32 +35,34 @@ function path_cmd.command(args)
    local path_sep = cfg.export_path_separator
 
    if args.lr_path then
-      util.printout(util.cleanup_path(lr_path, ';', cfg.lua_version))
+      util.printout(util.cleanup_path(lr_path, ';', cfg.lua_version, true))
       return true
    elseif args.lr_cpath then
-      util.printout(util.cleanup_path(lr_cpath, ';', cfg.lua_version))
+      util.printout(util.cleanup_path(lr_cpath, ';', cfg.lua_version, true))
       return true
    elseif args.lr_bin then
-      util.printout(util.cleanup_path(lr_bin, path_sep))
+      util.printout(util.cleanup_path(lr_bin, path_sep, nil, true))
       return true
    end
+
+   local clean_path = util.cleanup_path(os.getenv("PATH"), path_sep, nil, true)
    
    if args.append then
       lr_path = package.path .. ";" .. lr_path
       lr_cpath = package.cpath .. ";" .. lr_cpath
-      lr_bin = os.getenv("PATH") .. path_sep .. lr_bin
+      lr_bin = clean_path .. path_sep .. lr_bin
    else
       lr_path =  lr_path.. ";" .. package.path
       lr_cpath = lr_cpath .. ";" .. package.cpath
-      lr_bin = lr_bin .. path_sep .. os.getenv("PATH")
+      lr_bin = lr_bin .. path_sep .. clean_path
    end
    
    local lpath_var, lcpath_var = util.lua_path_variables()
 
-   util.printout(fs.export_cmd(lpath_var, util.cleanup_path(lr_path, ';', cfg.lua_version)))
-   util.printout(fs.export_cmd(lcpath_var, util.cleanup_path(lr_cpath, ';', cfg.lua_version)))
+   util.printout(fs.export_cmd(lpath_var, util.cleanup_path(lr_path, ';', cfg.lua_version, args.append)))
+   util.printout(fs.export_cmd(lcpath_var, util.cleanup_path(lr_cpath, ';', cfg.lua_version, args.append)))
    if not args.no_bin then
-      util.printout(fs.export_cmd("PATH", util.cleanup_path(lr_bin, path_sep)))
+      util.printout(fs.export_cmd("PATH", util.cleanup_path(lr_bin, path_sep, nil, args.append)))
    end
    return true
 end

src/luarocks/core/cfg.lua
@@ -763,8 +763,8 @@ function cfg.init(detected, warning)
 
    function cfg.init_package_paths()
       local lr_path, lr_cpath, lr_bin = cfg.package_paths()
-      package.path = util.cleanup_path(package.path .. ";" .. lr_path, ";", lua_version)
-      package.cpath = util.cleanup_path(package.cpath .. ";" .. lr_cpath, ";", lua_version)
+      package.path = util.cleanup_path(package.path .. ";" .. lr_path, ";", lua_version, true)
+      package.cpath = util.cleanup_path(package.cpath .. ";" .. lr_cpath, ";", lua_version, true)
    end
 
    --- Check if platform was detected

src/luarocks/core/util.lua
@@ -167,16 +167,16 @@ end
 -- @param list string: A path string (from $PATH or package.path)
 -- @param sep string: The separator
 -- @param lua_version (optional) string: The Lua version to use.
--- @param keep_last (optional) if true, keep last occurrence in case
--- of duplicates; otherwise keep first occurrence. The default is false.
-function util.cleanup_path(list, sep, lua_version, keep_last)
+-- @param keep_first (optional) if true, keep first occurrence in case
+-- of duplicates; otherwise keep last occurrence. The default is false.
+function util.cleanup_path(list, sep, lua_version, keep_first)
    assert(type(list) == "string")
    assert(type(sep) == "string")
    local parts = util.split_string(list, sep)
    local final, entries = {}, {}
    local start, stop, step
 
-   if keep_last then
+   if keep_first then
       start, stop, step = 1, #parts, 1
    else
       start, stop, step = #parts, 1, -1
@@ -192,11 +192,12 @@ function util.cleanup_path(list, sep, lua_version, keep_last)
          end)
       end
       if not entries[part] then
-         local at = keep_last and #final+1 or 1
+         local at = keep_first and #final+1 or 1
          table.insert(final, at, part)
          entries[part] = true
       end
    end
+
    return table.concat(final, sep)
 end
 