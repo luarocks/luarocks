luarocks/luarocks

commit 65c417e0ecda55f44c691df032163a8c08f0b52a

path, loader: handle coexisting foo.bar and foo.bar.init modules
LuaRocks used to conflate in its manifest two modules whose names only
differed by ".init" (e.g. "foo.bar" and "foo.bar.init"). With this change,
`path.path_to_modules` treats them as distinct modules, and `luarocks.loader`
handles them correctly (given `require("foo.bar")`, it looks for
`foo.bar` first and then `foo.bar.init` next).

src/luarocks/loader.lua
@@ -159,6 +159,22 @@ local function call_other_loaders(module, name, version, module_name)
    return "Failed loading module "..module.." in LuaRocks rock "..name.." "..version
 end
 
+local function add_providers(providers, entries, tree, module, filter_file_name)
+   for i, entry in ipairs(entries) do
+      local name, version = entry:match("^([^/]*)/(.*)$")
+      local file_name = tree.manifest.repository[name][version][1].modules[module]
+      if type(file_name) ~= "string" then
+         error("Invalid data in manifest file for module "..tostring(module).." (invalid data for "..tostring(name).." "..tostring(version)..")")
+      end
+      file_name = filter_file_name(file_name, name, version, tree.tree, i)
+      if loader.context[name] == version then
+         return name, version, file_name
+      end
+      version = vers.parse_version(version)
+      table.insert(providers, {name = name, version = version, module_name = file_name, tree = tree})
+   end
+end
+
 --- Search for a module in the rocks trees
 -- @param module string: module name (eg. "socket.core")
 -- @param filter_file_name function(string, string, string, string, number):
@@ -180,21 +196,16 @@ local function select_module(module, filter_file_name)
    end
 
    local providers = {}
+   local initmodule
    for _, tree in ipairs(loader.rocks_trees) do
       local entries = tree.manifest.modules[module]
       if entries then
-         for i, entry in ipairs(entries) do
-            local name, version = entry:match("^([^/]*)/(.*)$")
-            local file_name = tree.manifest.repository[name][version][1].modules[module]
-            if type(file_name) ~= "string" then
-               error("Invalid data in manifest file for module "..tostring(module).." (invalid data for "..tostring(name).." "..tostring(version)..")")
-            end
-            file_name = filter_file_name(file_name, name, version, tree.tree, i)
-            if loader.context[name] == version then
-               return name, version, file_name
-            end
-            version = vers.parse_version(version)
-            table.insert(providers, {name = name, version = version, module_name = file_name, tree = tree})
+         add_providers(providers, entries, tree, module, filter_file_name)
+      else
+         initmodule = initmodule or module .. ".init"
+         entries = tree.manifest.modules[initmodule]
+         if entries then
+            add_providers(providers, entries, tree, initmodule, filter_file_name)
          end
       end
    end

src/luarocks/core/path.lua
@@ -36,7 +36,7 @@ end
 
 --- Convert a pathname to a module identifier.
 -- In Unix, for example, a path "foo/bar/baz.lua" is converted to
--- "foo.bar.baz"; "bla/init.lua" returns "bla"; "foo.so" returns "foo".
+-- "foo.bar.baz"; "bla/init.lua" returns "bla.init"; "foo.so" returns "foo".
 -- @param file string: Pathname of module
 -- @return string: The module identifier, or nil if given path is
 -- not a conformant module path (the function does not check if the
@@ -47,10 +47,6 @@ function path.path_to_module(file)
    local name = file:match("(.*)%."..cfg.lua_extension.."$")
    if name then
       name = name:gsub("/", ".")
-      local init = name:match("(.*)%.init$")
-      if init then
-         name = init
-      end
    else
       name = file:match("(.*)%."..cfg.lib_extension.."$")
       if name then

commit e5e05b5c5308a7693bb75fc24dc070d0bcbd0569

fix regression in dependency matching of luarocks.loader
Regression introduced in
https://github.com/luarocks/luarocks/commit/65c417e0ecda55f44c691df032163a8c08f0b52a

Thanks to @lhemkendreis for the detailed description and suggested
fix, which was applied here!

Added a regression test which exercises dependency matching.

Closes #1175.

src/luarocks/loader.lua
@@ -149,12 +149,18 @@ local function select_module(module, filter_file_name)
    for _, tree in ipairs(tree_manifests) do
       local entries = tree.manifest.modules[module]
       if entries then
-         add_providers(providers, entries, tree, module, filter_file_name)
+         local n, v, f = add_providers(providers, entries, tree, module, filter_file_name)
+         if n then
+            return n, v, f
+         end
       else
          initmodule = initmodule or module .. ".init"
          entries = tree.manifest.modules[initmodule]
          if entries then
-            add_providers(providers, entries, tree, initmodule, filter_file_name)
+            local n, v, f = add_providers(providers, entries, tree, initmodule, filter_file_name)
+            if n then
+               return n, v, f
+            end
          end
       end
    end

spec/loader_spec.lua
@@ -1,10 +1,74 @@
 local test_env = require("spec.util.test_env")
 local run = test_env.run
+local testing_paths = test_env.testing_paths
+local write_file = test_env.write_file
 
 describe("luarocks.loader", function()
    describe("#unit", function()
       it("starts", function()
          assert(run.lua_bool([[-e "require 'luarocks.loader'; print(package.loaded['luarocks.loaded'])"]]))
+      end)
+   end)
+
+   describe("#integration", function()
+      it("respects version constraints", function()
+         test_env.run_in_tmp(function(tmpdir)
+            write_file("rock_b_01.lua", "print('ROCK B 0.1'); return {}", finally)
+            write_file("rock_b-0.1-1.rockspec", [[
+               package = "rock_b"
+               version = "0.1-1"
+               source = {
+                  url = "file://]] .. tmpdir:gsub("\\", "/") .. [[/rock_b_01.lua"
+               }
+               build = {
+                  type = "builtin", 
+                  modules = {
+                     rock_b = "rock_b_01.lua"
+                  }
+               }
+            ]], finally)
+   
+            write_file("rock_b_10.lua", "print('ROCK B 1.0'); return {}", finally)
+            write_file("rock_b-1.0-1.rockspec", [[
+               package = "rock_b"
+               version = "1.0-1"
+               source = {
+                  url = "file://]] .. tmpdir:gsub("\\", "/") .. [[/rock_b_10.lua"
+               }
+               build = {
+                  type = "builtin", 
+                  modules = {
+                     rock_b = "rock_b_10.lua"
+                  }
+               }
+            ]], finally)
+   
+            write_file("rock_a.lua", "require('rock_b'); return {}", finally)
+            write_file("rock_a-2.0-1.rockspec", [[
+               package = "rock_a"
+               version = "2.0-1"
+               source = {
+                  url = "file://]] .. tmpdir:gsub("\\", "/") .. [[/rock_a.lua"
+               }
+               dependencies = {
+                  "rock_b < 1.0",
+               }
+               build = {
+                  type = "builtin", 
+                  modules = {
+                     rock_a = "rock_a.lua"
+                  }
+               }
+            ]], finally)
+
+            print(run.luarocks("make --server=" .. testing_paths.fixtures_dir .. "/a_repo --tree=" .. testing_paths.testing_tree .. " ./rock_b-0.1-1.rockspec"))
+            print(run.luarocks("make --server=" .. testing_paths.fixtures_dir .. "/a_repo --tree=" .. testing_paths.testing_tree .. " ./rock_b-1.0-1.rockspec --keep"))
+            print(run.luarocks("make --server=" .. testing_paths.fixtures_dir .. "/a_repo --tree=" .. testing_paths.testing_tree .. " ./rock_a-2.0-1.rockspec"))
+   
+            local output = run.lua([[-e "require 'luarocks.loader'; require('rock_a')"]])
+   
+            assert.matches("ROCK B 0.1", output, 1, true)
+         end)
       end)
    end)
 end)