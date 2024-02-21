57447428998c53caf0ac9106016188c8570d4827
Hisham Muhammad <hisham@gobolinux.org>
2024-02-21 01:23:59 -0300

    fix: handle error if test.command is not a string.
    Fixes #1055.

--- src/luarocks/test/command.lua
+++ src/luarocks/test/command.lua
@@ -27,12 +27,18 @@ function command.run_tests(test, args)
    local ok
 
    if test.script then
+      if type(test.script) ~= "string" then
+         return nil, "Malformed rockspec: 'script' expects a string"
+      end
       if not fs.exists(test.script) then
          return nil, "Test script " .. test.script .. " does not exist"
       end
       local lua = fs.Q(cfg.variables["LUA"])  -- get lua interpreter configured
       ok = fs.execute(lua, test.script, unpack(args))
    elseif test.command then
+      if type(test.command) ~= "string" then
+         return nil, "Malformed rockspec: 'command' expects a string"
+      end
       ok = fs.execute(test.command, unpack(args))
    end
 
