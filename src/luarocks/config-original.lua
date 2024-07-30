3d2135842a703edae632f4623f466c0c8132ffb2
V1K1NGbg <victor@ilchev.com>
2024-07-30 21:00:26 +0300

    converted config

--- src/luarocks/config-original.lua
+++ src/luarocks/config-original.lua
@@ -1,4 +1,4 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local pairs = _tl_compat and _tl_compat.pairs or pairs; local config = {}
+local config = {}
 
 local persist = require("luarocks.persist")
 
@@ -10,8 +10,6 @@ local cfg_skip = {
    upload_servers = true,
 }
 
-
-
 function config.should_skip(k, v)
    return type(v) == "function" or cfg_skip[k]
 end
@@ -19,7 +17,7 @@ end
 local function cleanup(tbl)
    local copy = {}
    for k, v in pairs(tbl) do
-      if not (type(k) == "string" and config.should_skip(k, v)) then
+      if not config.should_skip(k, v) then
          copy[k] = v
       end
    end
