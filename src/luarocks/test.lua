9d4bebd145cbdf3185ed68ba1eb4140303eb093f
V1K1NGbg <victor@ilchev.com>
2024-08-13 19:53:11 +0300

    test

--- src/luarocks/test.lua
+++ src/luarocks/test.lua
@@ -1,9 +1,16 @@
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local pcall = _tl_compat and _tl_compat.pcall or pcall; local table = _tl_compat and _tl_compat.table or table; local test = {}
 
-local test = {}
 
 local fetch = require("luarocks.fetch")
 local deps = require("luarocks.deps")
 local util = require("luarocks.util")
+
+
+
+
+
+
+
 
 local test_types = {
    "busted",
@@ -11,12 +18,19 @@ local test_types = {
 }
 
 local test_modules = {}
+local typetomod = {}
+local modtotype = {}
 
 for _, test_type in ipairs(test_types) do
-   local mod = require("luarocks.test." .. test_type)
+   local mod
+   if test_type == "command" then
+      mod = require("luarocks.test.command")
+   elseif test_type == "busted" then
+      mod = require("luarocks.test.busted")
+   end
    table.insert(test_modules, mod)
-   test_modules[test_type] = mod
-   test_modules[mod] = test_type
+   typetomod[test_type] = mod
+   modtotype[mod] = test_type
 end
 
 local function get_test_type(rockspec)
@@ -26,14 +40,14 @@ local function get_test_type(rockspec)
 
    for _, test_module in ipairs(test_modules) do
       if test_module.detect_type() then
-         return test_modules[test_module]
+         return modtotype[test_module]
       end
    end
 
    return nil, "could not detect test type -- no test suite for " .. rockspec.package .. "?"
 end
 
--- Run test suite as configured in rockspec in the current directory.
+
 function test.run_test_suite(rockspec_arg, test_type, args, prepare)
    local rockspec
    if type(rockspec_arg) == "string" then
@@ -43,13 +57,12 @@ function test.run_test_suite(rockspec_arg, test_type, args, prepare)
          return nil, err, errcode
       end
    else
-      assert(type(rockspec_arg) == "table")
       rockspec = rockspec_arg
    end
 
    if not test_type then
       local err
-      test_type, err = get_test_type(rockspec, test_type)
+      test_type, err = get_test_type(rockspec)
       if not test_type then
          return nil, err
       end
@@ -62,23 +75,30 @@ function test.run_test_suite(rockspec_arg, test_type, args, prepare)
       "test_dependencies",
    }
    for _, dep_kind in ipairs(all_deps) do
-      if rockspec[dep_kind] and next(rockspec[dep_kind]) then
-         local ok, err, errcode = deps.fulfill_dependencies(rockspec, dep_kind, "all")
+      if (rockspec)[dep_kind] and next((rockspec)[dep_kind]) ~= nil then
+         local _, err, errcode = deps.fulfill_dependencies(rockspec, dep_kind, "all")
          if err then
             return nil, err, errcode
          end
       end
    end
 
-   local mod_name = "luarocks.test." .. test_type
-   local pok, test_mod = pcall(require, mod_name)
-   if not pok then
-      return nil, "failed loading test execution module " .. mod_name
+   local pok, test_mod
+   if test_type == "command" then
+      pok, test_mod = pcall(require, "luarocks.test.command")
+      if not pok then
+         return nil, "failed loading test execution module luarocks.test.command"
+      end
+   elseif test_type == "busted" then
+      pok, test_mod = pcall(require, "luarocks.test.busted")
+      if not pok then
+         return nil, "failed loading test execution module luarocks.test.busted"
+      end
    end
 
    if prepare then
       if test_type == "busted" then
-         return test_mod.run_tests(rockspec_arg, {"--version"})
+         return test_mod.run_tests(rockspec.test, { "--version" })
       else
          return true
       end
@@ -87,7 +107,7 @@ function test.run_test_suite(rockspec_arg, test_type, args, prepare)
       if type(flags) == "table" then
          util.variable_substitutions(flags, rockspec.variables)
 
-         -- insert any flags given in test.flags at the front of args
+
          for i = 1, #flags do
             table.insert(args, i, flags[i])
          end
