c64e6b23b630c52ed28f581376cdb10c9360fd54
V1K1NGbg <victor@ilchev.com>
2024-08-15 19:25:34 +0300

    build_cmake, build_command

--- src/luarocks/build/originals/command-original.lua
+++ src/luarocks/build/originals/command-original.lua
@@ -1,23 +1,17 @@
 
-
+--- Build back-end for raw listing of commands in rockspec files.
 local command = {}
-
 
 local fs = require("luarocks.fs")
 local util = require("luarocks.util")
 local cfg = require("luarocks.core.cfg")
 
-
-
-
-
-
-
-
-
-
-
+--- Driver function for the "command" build back-end.
+-- @param rockspec table: the loaded rockspec.
+-- @return boolean or (nil, string): true if no errors occurred,
+-- nil and an error message otherwise.
 function command.run(rockspec, not_install)
+   assert(rockspec:type() == "rockspec")
 
    local build = rockspec.build
 
@@ -25,8 +19,8 @@ function command.run(rockspec, not_install)
 
    local env = {
       CC = cfg.variables.CC,
-
-
+      --LD = cfg.variables.LD,
+      --CFLAGS = cfg.variables.CFLAGS,
    }
 
    if build.build_command then
