b9b598374a73d77fb456bb087df993e3754252b5
V1K1NGbg <victor@ilchev.com>
2024-08-16 17:58:37 +0300

    download

--- src/luarocks/download.lua
+++ src/luarocks/download.lua
@@ -1,4 +1,5 @@
-local download = {}
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local pairs = _tl_compat and _tl_compat.pairs or pairs; local download = {}
+
 
 local path = require("luarocks.path")
 local fetch = require("luarocks.fetch")
@@ -18,51 +19,58 @@ local function get_file(filename)
          return nil, err
       end
    else
-      -- discard third result
+
       local ok, err = fetch.fetch_url(filename)
       return ok, err
    end
 end
 
-function download.download(arch, name, namespace, version, all, check_lua_versions)
-   local substring = (all and name == "")
+function download.download_all(arch, name, namespace, version)
+   local substring = (name == "")
    local query = queries.new(name, namespace, version, substring, arch)
    local search_err
 
-   if all then
-      local results = search.search_repos(query)
-      local has_result = false
-      local all_ok = true
-      local any_err = ""
-      for name, result in pairs(results) do  -- luacheck: ignore 422
-         for version, items in pairs(result) do  -- luacheck: ignore 422
-            for _, item in ipairs(items) do
-               -- Ignore provided rocks.
-               if item.arch ~= "installed" then
-                  has_result = true
-                  local filename = path.make_url(item.repo, name, version, item.arch)
-                  local ok, err = get_file(filename)
-                  if not ok then
-                     all_ok = false
-                     any_err = any_err .. "\n" .. err
-                  end
+   local results = search.search_repos(query)
+   local has_result = false
+   local all_ok = true
+   local any_err = ""
+   for name, result in pairs(results) do
+      for version, items in pairs(result) do
+         for _, item in ipairs(items) do
+
+            if item.arch ~= "installed" then
+               has_result = true
+               local filename = path.make_url(item.repo, name, version, item.arch)
+               local ok, err = get_file(filename)
+               if not ok then
+                  all_ok = false
+                  any_err = any_err .. "\n" .. err
                end
             end
          end
       end
-
-      if has_result then
-         return all_ok, any_err
-      end
-   else
-      local url
-      url, search_err = search.find_rock_checking_lua_versions(query, check_lua_versions)
-      if url then
-         return get_file(url)
-      end
    end
+
+   if has_result then
+      return all_ok, any_err
+   end
+
    local rock = util.format_rock_name(name, namespace, version)
-   return nil, "Could not find a result named "..rock..(search_err and ": "..search_err or ".")
+   return nil, "Could not find a result named " .. rock .. (search_err and ": " .. search_err or ".")
+end
+
+function download.download_file(arch, name, namespace, version, check_lua_versions)
+   local query = queries.new(name, namespace, version, false, arch)
+   local search_err
+
+   local url
+   url, search_err = search.find_rock_checking_lua_versions(query, check_lua_versions)
+   if url then
+      return get_file(url)
+   end
+
+   local rock = util.format_rock_name(name, namespace, version)
+   return nil, "Could not find a result named " .. rock .. (search_err and ": " .. search_err or ".")
 end
 
 return download
