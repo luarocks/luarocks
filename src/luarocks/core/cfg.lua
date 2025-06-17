a8fcea6c0a3443ff31f38f026d29a92098ffeb83
Hisham Muhammad <hisham@gobolinux.org>
2025-06-17 19:13:20 -0300

    Release 3.12.1

--- src/luarocks/core/cfg.lua
+++ src/luarocks/core/cfg.lua
@@ -21,7 +21,7 @@ local vers = require("luarocks.core.vers")
 
 --------------------------------------------------------------------------------
 
-local program_version = "3.12.0"
+local program_version = "3.12.1"
 
 local is_windows = package.config:sub(1,1) == "\\"
 
